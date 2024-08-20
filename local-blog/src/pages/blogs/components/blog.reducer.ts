bfb41c295e96675d57a6ddd858b0b25be5e8c1ab
Luong Tu Nhan <tunhan281204@gmail.com>
2024-08-20 23:00:53 +0700

    feat: completed redux toolkit w typescript

--- local-blog/src/pages/blogs/components/blog.reducer.ts
+++ local-blog/src/pages/blogs/components/blog.reducer.ts
@@ -1,10 +1,59 @@
-import { createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, current } from "@reduxjs/toolkit";
 import { Post } from "../../../types/blog.type";
+import { initialPostList } from "../../../constants/blog";
 interface BlogState {
   postList: Post[];
+  editingPost: Post | null;
 }
 const initialState: BlogState = {
-  postList: [],
+  postList: initialPostList,
+  editingPost: null,
 };
-const blogReducer = createReducer(initialState, (builder) => {});
+export const addPost = createAction<Post>("blog/addPost");
+export const deletePost = createAction<string>("blog/deletePost");
+export const startEditingPost = createAction<string>("blog/startEditingPost");
+export const cancelEditingPost = createAction("blog/cancelEditingPost");
+export const finishEditingPost = createAction<Post>("blog/finishEditingPost");
+const blogReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(addPost, (state, action) => {
+      const post = action.payload;
+      state.postList.push(post);
+    })
+    .addCase(deletePost, (state, action) => {
+      const postId = action.payload;
+      const foundPostIndex: number = state.postList.findIndex(
+        (post) => post.id === postId
+      );
+      if (foundPostIndex !== -1) {
+        state.postList.splice(foundPostIndex, 1);
+      }
+    })
+    .addCase(startEditingPost, (state, action) => {
+      const postId = action.payload;
+      const foundPost =
+        state.postList.find((post) => post.id === postId) || null;
+      state.editingPost = foundPost;
+    })
+    .addCase(cancelEditingPost, (state) => {
+      state.editingPost = null;
+    })
+    .addCase(finishEditingPost, (state, action) => {
+      const postId = action.payload.id;
+      state.postList.some((post, index) => {
+        if (post.id === postId) {
+          state.postList[index] = action.payload;
+          return true;
+        }
+        return false;
+      });
+      state.editingPost = null;
+    })
+    .addMatcher(
+      (action) => action.type.includes("cancel"),
+      (state, action) => {
+        console.log(current(state));
+      }
+    );
+});
 export default blogReducer;
